7d24814a91f594881ffb87a0585062debc816e30
Kevin <duong.kev@gmail.com>
2017-03-10 22:42:12 -0800

    Adding ToDo list under Cards

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,16 +3,16 @@ import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
 import Home from './containers/Home';
 import Profile from './containers/Profile';
+import Card from './containers/Card.jsx';
+
 
 export default () => {
   return (
     <Route path="/" component={App}>
       <IndexRoute component={Home}/>
       <Route path="/profile" component={Profile}/>
+      <Route path="/card" component={Card}/>
     </Route>
-
-
-
 
   );
 };
